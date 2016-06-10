b1894c86bc9ef9e6d1fb2eb0a79d2c5f767d7538
elmatematic0 <wkdalebout+github@gmail.com>
2016-06-10 15:34:53 -0400

    creating edit form that communicates with spreadsheet. making small modifications to the show layout. need to figure out callback function on edit before redirecting back to show.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,35 @@ class ArticlesController < ApplicationController
     articles_json = open("https://script.google.com/macros/s/AKfycbwmXKhtyX_IGhHr0jCJ4EKNEN81sl4R44ZY67dIZSrurVoYml3b/exec").read
     @articles = JSON.parse(articles_json)
   end
+def show
+    articles_json = open("https://script.google.com/macros/s/AKfycbwmXKhtyX_IGhHr0jCJ4EKNEN81sl4R44ZY67dIZSrurVoYml3b/exec").read
+    articles_hash = JSON.parse(articles_json)
+    @article = articles_hash['results'][params[:id].to_i - 1] 
+end
+
+ 
+  def new
+    
+  end
+
+  # GET /collections/1/edit
+  def edit
+    articles_json = open("https://script.google.com/macros/s/AKfycbwmXKhtyX_IGhHr0jCJ4EKNEN81sl4R44ZY67dIZSrurVoYml3b/exec").read
+    articles_hash = JSON.parse(articles_json)
+    @article = articles_hash['results'][params[:id].to_i - 1]
+  end
+
+  # POST /collections
+  # POST /collections.json
+  def create
+  
+  end 
+
+  # PATCH/PUT /collections/1
+  # PATCH/PUT /collections/1.json
+  def update
+    
+  end
 
   def show
     articles_json = open("https://script.google.com/macros/s/AKfycbwmXKhtyX_IGhHr0jCJ4EKNEN81sl4R44ZY67dIZSrurVoYml3b/exec").read
